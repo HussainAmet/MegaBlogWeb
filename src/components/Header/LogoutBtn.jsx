a06dc22ce77da8ec53b2a0ffca6fb214d9051f35
Hussain Amet <hussainamet@gmail.com>
2023-12-25 16:37:04 +0530

    reload on logout

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -5,13 +5,14 @@ import { logout } from '../../store/authSlice'
 
 function LogoutBtn() {
     const dispatch = useDispatch()
-    const logoutHandler = () => {
-        authService.logout()
+    const logoutHandler = async () => {
+        await authService.logout()
         .then(() => {
             dispatch(logout())
-        })
+        });
+        window.location.reload(false);
     }
-  return <button onClick={logoutHandler} className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>;
+    return <button onClick={logoutHandler} className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>;
 }
 
 export default LogoutBtn
