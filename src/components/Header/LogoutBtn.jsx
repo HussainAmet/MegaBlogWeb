2ab436ccd3b2090d368d78962d335cf9ae76c8da
Hussain Amet <hussainamet@gmail.com>
2023-12-25 19:21:42 +0530

    logout to login

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -13,7 +13,7 @@ function LogoutBtn() {
         .then(() => {
             dispatch(logout())
         });
-        navigate("/")
+        navigate("/login")
         reload();
     }
     return <button onClick={logoutHandler} className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>;
