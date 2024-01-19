7fb5e8d9fb3d347331ca03f1c96da2263a9975e5
Hussain Amet <hussainamet@gmail.com>
2024-01-19 16:22:42 +0530

    bug fix

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import service from '../appwrite/conf'
 import { Link } from 'react-router-dom'
-import { reload } from '../reload'
 
 function PostCard({$id, title, featuredImage, status = "active"}) {
   return (
@@ -10,9 +9,9 @@ function PostCard({$id, title, featuredImage, status = "active"}) {
         {status === "inactive" ? <h2 className='text-xl text-red-600 font-bold mb-4'>{status}</h2> : null}
         <div className='flex-col w-full justify-center mb-4'>
           <div className='mb-4'>
-            {featuredImage? <img src={service.getFilepreview(featuredImage)} alt={title} className='rounded-xl' /> : reload()}
+            {featuredImage? <img src={service.getFilepreview(featuredImage)} alt={title} className='rounded-xl' /> : <img src="../../assets/404.jpg" alt="404" /> }
           </div>
-          <h2 className='text-xl font-bold'>{title}</h2>
+          <h2 className='text-xl font-bold'>{title ? title : "Post Not Found :("}</h2>
         </div>
       </div>
     </Link>
